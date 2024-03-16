5a44e4d1fd84a31b2eb3be6e4582bd69d22671cc
mgrjyoti <magarjyotis1304@gmail.com>
2024-03-16 15:33:03 +0530

    CodSoft Task

---
+++ quizApplication/src/com/codSoft/quiz/QuizQuestion.java
@@ -0,0 +1,27 @@
+package com.codSoft.quiz;
+
+import java.util.List;
+
+public class QuizQuestion {
+	private List<String> options;
+    private int correctAnswerIndex;
+    private String question;
+
+    public QuizQuestion(String question, List<String> options, int correctAnswerIndex) {
+        this.question = question;
+        this.options = options;
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public int getCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
+}
